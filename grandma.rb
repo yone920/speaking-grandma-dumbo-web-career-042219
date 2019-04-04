178138694152f784c150870bff48d6300a4ceeac
Yonas Fesehatsion <yone920@gmail.com>
2019-04-04 21:04:54 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(speak)
   if speak == speak.downcase
     "HUH?! SPEAK UP, SONNY!"
 
-  elsif speak == "I LOVE YOU GRANDMA!".upcase
+  elsif speak == speak.upcase
     "NO, NOT SINCE 1938!"
     
   elsif speak == "I LOVE YOU GRANDMA" 
